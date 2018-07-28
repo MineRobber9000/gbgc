af793bc4f97a18a01f811bb9c0a80eee8df57ed1
MineRobber9000 <khuxkm@tilde.team>
2018-07-28 07:42:12 -0400

    Implement verbosity argument

--- bgb/connect.py
+++ bgb/connect.py
@@ -1,6 +1,7 @@
 import argparse,bgblink,time
 from lumberjack import log as log_important
 
+global VERBOSE
 VERBOSE = False
 
 def log(*args):
@@ -58,9 +59,11 @@ def gbgc(x,o):
 if __name__=="__main__":
 	try:
 		parser = argparse.ArgumentParser(description="Connects BGB to the GBGC protocol and server.",epilog="Host and port default to 127.0.0.1:8765.")
+		parser.add_argument("--verbose","-v",action="store_true",help="Whether or not this program should print pretty logs or not.")
 		parser.add_argument("host",default="127.0.0.1",nargs="?",help="Host to connect to.")
 		parser.add_argument("port",default=8765,type=int,nargs="?",help="Port to connect to.")
 		args = parser.parse_args()
+		VERBOSE = args.verbose
 		log("Connecting to host \"{}\" and port {!s}".format(args.host,args.port))
 		link = bgblink.BGBLinkCable(args.host,args.port)
 		link.setExchangeHandler(gbgc)
@@ -70,5 +73,6 @@ if __name__=="__main__":
 		while True:
 			time.sleep(10)
 	except Exception as e:
+		VERBOSE = True
 		log("An error occurred: \"{}: {!s}\"".format(e.__class__.__name__,e),"X")
 		pass
