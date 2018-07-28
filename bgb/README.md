c1907798b426b16147d0ebaf1df9f19c06db923e
MineRobber9000 <khuxkm@tilde.team>
2018-07-28 06:30:36 -0400

    Add retrieve function

--- bgb/README.md
+++ bgb/README.md
@@ -4,6 +4,18 @@ This program will allow any user of the BGB emulator to use GBGC services. It al
 
 ## Protocol
 
+### Commands
+
 |Command|ID|Has contents?|Contents|Response|
 |-|-|-|-|-|
-|`PING`|`00`|No|N/A|`00`|
+|`PING`|`00`|No|N/A|`PING`|
+|`RETR`|`01`|Yes|URL of content, null terminated|`ACK` until recieving null, then `ACK_FIN`, then the data (with `END_PACKET` at the end of the data).|
+
+### Responses
+
+|Response|ID|Explanation|
+|-|-|-|
+|`PING`|`00`|Sent in response to a `PING` command. Can, in theory, be any non-FF value.|
+|`ACK`|`01`|Sent as an acknowledgement of recieved data.|
+|`ACK_FIN`|`02`|Used to acknowledge the end of a sequence of data.|
+|`END_PACKET`|`FF`|Like `ACK_FIN`, but where not allowing the use of `02` does not make sense. (For example, in the transfer of program code.)|
--- bgb/connect.py
+++ bgb/connect.py
@@ -1,17 +1,59 @@
 import argparse,bgblink,time
-from lumberjack import log
+from lumberjack import log as log_important
 
 VERBOSE = False
 
-COMMANDS = {0x00: "PING"}
+def log(*args):
+	if not VERBOSE:
+		return
+	log_important(*args)
+
+STANDBY, RECV_NAME, SEND_DATA = range(3)
+
+STATE = STANDBY
+
+NAME = ""
+DATA = []
+
+def getData(loc):
+	global DATA
+	DATA = [ord(x) for x in loc]
+
+COMMANDS = {0x00: "PING",0x01:"RETR"}
 
 def gbgc(x,o):
-	log("{:02X} {}".format(x,COMMANDS.get(x,"INVALID")),">","cyan")
-	if x==0x00:
-		log("00 PING","<","cyan")
-		return x
-	log("FF INVALID","<","red")
-	return 0xFF
+	global STATE, NAME, DATA
+	if STATE==STANDBY:
+		log("{:02X} {}".format(x,COMMANDS.get(x,"INVALID")),">","cyan")
+		if x==0x00:
+			log("00 PING","<","cyan")
+			return x
+		elif x==0x01:
+			log("01 ACK, state switch to RECV_NAME","<","green")
+			STATE = RECV_NAME
+			NAME = ""
+			return 0x01
+		log("FF INVALID","<","red")
+		return 0xFF
+	elif STATE==RECV_NAME:
+		log("{:02X} {!s}".format(x,(chr(x) if x!=0x00 else "<END>")),">","cyan")
+		if x==0x00:
+			log("02 ACK_FIN, state switch to SEND_DATA","<","green")
+			STATE = SEND_DATA
+			getData(NAME)
+			return 0x02
+		NAME += chr(x)
+		log("01 ACK","<","yellow")
+		return 0x01
+	elif STATE==SEND_DATA:
+		if len(DATA)==0:
+			log("FF END_PACKET, state switch to STANDBY","<","green")
+			STATE=STANDBY
+			return 0xFF
+		r = DATA.pop(0)
+#		log("{:02X}".format(x),">","cyan")
+		log("{:02X}".format(r),"<","green")
+		return r
 
 if __name__=="__main__":
 	try:
